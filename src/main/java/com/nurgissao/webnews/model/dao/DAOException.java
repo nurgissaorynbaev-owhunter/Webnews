63a48635b72687c7d63dee52b5463d2fa60bb81d
Nurgissa Orynbaev <onurgissaa@gmail.com>
2017-01-15 00:01:25 +0600

    git add daoException, User

--- src/main/java/com/nurgissao/webnews/model/dao/DAOException.java
+++ src/main/java/com/nurgissao/webnews/model/dao/DAOException.java
@@ -1,4 +1,16 @@
 package com.nurgissao.webnews.model.dao;
 
-public class DAOException {
+public class DAOException extends Exception {
+
+    public DAOException(String message) {
+        super(message);
+    }
+
+    public DAOException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DAOException(Throwable cause) {
+        super(cause);
+    }
 }
--- src/main/java/com/nurgissao/webnews/model/entity/User.java
+++ src/main/java/com/nurgissao/webnews/model/entity/User.java
@@ -1,4 +1,72 @@
 package com.nurgissao.webnews.model.entity;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String login;
+    private String email;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", login='" + login + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
