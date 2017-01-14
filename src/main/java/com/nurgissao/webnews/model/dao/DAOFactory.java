71e59661ef0338d9cc0963f4bd0ba9683ee42295
Nurgissa Orynbaev <onurgissaa@gmail.com>
2017-01-15 00:02:16 +0600

    throw exception in default mode

--- src/main/java/com/nurgissao/webnews/model/dao/DAOFactory.java
+++ src/main/java/com/nurgissao/webnews/model/dao/DAOFactory.java
@@ -1,4 +1,15 @@
 package com.nurgissao.webnews.model.dao;
 
-public class DAOFactory {
+public abstract class DAOFactory {
+
+    public static DAOFactory getDAOFactory(DataSourceType type) throws DAOException {
+        switch (type) {
+            case H2:
+                return new H2DAOFactory();
+            default:
+                throw new DAOException("Not such type of data source.");
+        }
+    }
+
+    public abstract UserDAO getUserDAO();
 }
